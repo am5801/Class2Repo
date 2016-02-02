395a5fa90bf13bdbe45e11a949c04a2cf03a351d
am5801 <am5801@nyu.edu>
2016-02-02 11:58:02 -0500

    First commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$('#alert').click( function(){
+ alert('You clicked me!');
+});
+
+$('#makeGreen').click(function(){
+	$('#myFirstDiv').css('background-color','green')
+});
+
+$('#makeBlue').click(function(){
+	$('#myFirstDiv').css('background-color','blue');
+});
+
+$('#makeRed').click(function(){
+	$('#myFirstDiv').css('background-color','red')
+});
+
+var myNumber = 1;
+
+$('#addOne').click(
+	function() {
+  	myNumber = myNumber + 1;    
+  	$('#myNumber').text(myNumber);
+  }
+);
+
+$('#subtractOne').click(
+	function() {
+  	myNumber = myNumber - 1;    
+  	$('#myNumber').text(myNumber);
+  }
+);
